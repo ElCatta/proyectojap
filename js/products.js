f443bb244e4ff1d6b00907f5bf604f75c0a8d5fb
ElCatta <cattalurdai@gmail.com>
2022-10-11 12:11:09 -0300

    input count started

--- js/products.js
+++ js/products.js
@@ -89,7 +89,7 @@ function showProducts(list) {
                         </div>
                     </div>
                 </div>
-                <button type="button" onClick="addToCart(`+ product.id + `)" class="rightbot btn btn-warning btn-sm m-3"><h6>Añadir al carrito <i class="fa fa-cart-plus" aria-hidden="true"></i></h6></button>  
+                <button type="button" onclick="addToLocalCart(`+ product.id + `,1)" class="rightbot btn btn-warning btn-sm m-3"><h6>Añadir 1 al carrito <i class="fa fa-cart-plus" aria-hidden="true"></i></h6></button>  
             </div>
             `
     }
