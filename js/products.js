a8b55a0355b4eb0b389efd013c9482ac6b4ad913
ElCatta <cattalurdai@gmail.com>
2022-10-16 22:09:44 -0300

    currency order fixed

--- js/products.js
+++ js/products.js
@@ -79,7 +79,7 @@ function showProducts(list) {
                         <img style="border:none;" src="${product.image}" alt="product image" class="img-thumbnail border-0 ">
                     </div>
                     <div class="col-12">
-                        <h3 class="mb-1"> <strong> $${product.cost} ${product.currency}</strong></h3>     
+                        <h3 class="mb-1"> <strong> ${product.currency} $${product.cost} </strong></h3>     
                     </div>
                     <div class="col-7 mt-2">
                         <h5 class="font-weight-strong">${product.name}</h4>     
